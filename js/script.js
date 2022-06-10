0505a86273f335afc9c4d4305f9f33fed1f3819f
m7mmad <muhammadaaa727@gmial.com>
2022-06-10 19:30:54 +0300

    create typing auto

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// script
+
+var typed = new Typed(".typing", {
+    strings:["", "Web Designer", "", "Graphic Designer", "", "Web Developer", "", "YouTuber", ""],
+    typeSpeed:110,
+    BackSpeed:50,
+    loop:true
+})
