77de1f39d95c7324e5ed9c11392fd287f965c2bf
M7MMAD-OMAR <73592103+M7MMAD-OMAR@users.noreply.github.com>
2022-06-29 17:57:47 +0300

    Add logo light and dark

--- js/main.js
+++ js/main.js
@@ -1,35 +1,54 @@
 // theme dark and light mode
 
 const dayNight = document.querySelector(".day-night");
+const logo_dark = document.querySelector(".logo-img-dark");
+const logo_white = document.querySelector(".logo-img-white");
+
 dayNight.addEventListener("click", () => {
     dayNight.querySelector("i").classList.toggle("fa-sun");
     dayNight.querySelector("i").classList.toggle("fa-moon");
     document.body.classList.toggle("dark")
+    if (logo_white.classList.contains("hidden")) {
+        logo_white.classList.remove("hidden");
+        logo_dark.classList.add("hidden");
+    } else {
+        logo_white.classList.add("hidden");
+        logo_dark.classList.remove("hidden");
+    }
 
 })
+
+
+
 window.addEventListener("load", () => {
-    if(document.body.classList.contains("dark")) {
+    if (document.body.classList.contains("dark")) {
         dayNight.querySelector("i").classList.add("fa-sun");
+
     }
     else {
         dayNight.querySelector("i").classList.add("fa-moon");
-        
+        // logo_dark.classList.remove("hidden");
+        // logo_white.classList.add("hidden");
+
     }
-})
-// color: var(--bg-black-100);
-// script
 
+})
+
+
+// JS typing
 var typed = new Typed(".typing", {
-    strings:["", "Full Static Developer", "", "Web Designer", "", "Web Developer", "", "Graphic Designer", ""],
-    typeSpeed:110,
-    BackSpeed:50,
-    loop:true
+    strings: ["", "Full Static Developer", "", "Web Designer", "", "Web Developer", "", "Graphic Designer", ""],
+    typeSpeed: 110,
+    BackSpeed: 50,
+    loop: true
 })
 
+
+// Navbar change color by scroll and click
 const actives = document.querySelectorAll(".nav li a");
 
 actives.forEach((ele) => {
-    ele.onclick = function() {
+    ele.onclick = function () {
         actives.forEach((ele) => {
             ele.classList.remove("active");
         })
@@ -38,7 +57,8 @@ actives.forEach((ele) => {
 });
 
 
-window.onscroll = function() {
+
+window.onscroll = function () {
     let sY = window.scrollY;
     if (sY <= 715) {
         actives.forEach((ele) => {
@@ -58,7 +78,7 @@ window.onscroll = function() {
         })
         actives[2].classList.add("active");
     }
-    if (sY >= 3300) {
+    if (sY >= 3250) {
         actives.forEach((ele) => {
             ele.classList.remove("active");
         })
